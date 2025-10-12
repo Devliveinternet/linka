8039f42e8303479107effc0ce490bc14fe8bfbf5
thiago <thiago020698@gmail.com>
2025-10-12 18:23:00 -0300

    adiciona ao servidor vite o host:true

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/traccar': 'http://localhost:3000' } },
+  server: { proxy: { '/traccar': 'http://localhost:3000' },
+            host: true,
+            port: 5173
+},
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
