78e9e7db309f701aedc78167653c5a21e6a824c5
Thiago Caixeta de Souza <thiagocaixetadesouza10@gmail.com>
2025-10-14 22:52:18 -0300

    Reset Google Maps loader when API key changes

--- src/utils/googleMaps.ts
+++ src/utils/googleMaps.ts
@@ -1,2 +1,13 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
 export const GOOGLE_MAPS_SCRIPT_ID = '__googleMapsScriptId';
 export const GOOGLE_MAPS_LIBRARIES = ['maps', 'marker'] as const;
+
+type LoaderWithInstance = typeof Loader & { instance?: InstanceType<typeof Loader> | undefined };
+
+export function resetGoogleMapsLoaderInstance() {
+  const loaderConstructor = Loader as LoaderWithInstance;
+  if ('instance' in loaderConstructor) {
+    loaderConstructor.instance = undefined;
+  }
+}
